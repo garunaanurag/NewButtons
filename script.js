10b6f0cc765301afdc97044f4f68522476815879
garunaanurag <105156495+garunaanurag@users.noreply.github.com>
2023-09-01 16:10:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// const Rect1 = document.getElementById('rect1');
+// const rect2 = document.getElementById('rect2');
+// const button1 = document.getElementById('button1');
+// const button2 = document.getElementById('button2');
+// const button3 = document.getElementById('button3');
+
+// button1.addEventListener('click', () => {
+//   square1.style.backgroundColor = 'red';
+// });
+
+// button2.addEventListener('click', () => {
+//   square2.style.backgroundColor = 'green';
+// });
+
+// button3.addEventListener('click', () => {
+//   square1.style.backgroundColor = 'black';
+//   square2.style.backgroundColor = 'black';
+// });
+
+// function showRect1() {
+//   if (show.style.display === "none") {show.stlye.display = "block"}
+//   else {
+//     show.style.display = "none"
+//   }
+
+
+// }
+
+
+
